5fd26cbe942883396aff3962b792c1228cb6e573
Glenn Eggleton <geggleto@gmail.com>
2015-12-21 08:52:59 -0500

    First Commit

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# helper_classes
+# TheSlimCollective  Helper Classes
+Slim 3 is a very different beast than Slim 2.
+These Helper classes will help you migrate or start a new project in Slim 3.
+ 
+ 
+## BaseAction Example
+
+```php
+use \Your\Namespace;
+
+class HelloWorldAction extends TheSlimCollective\Helper\BaseAction {
+
+    public function __construct(ContainerInterface $containerInterface)
+    {
+        parent::__construct($containerInterface);
+    }
+    
+    public function __invoke (ServerRequestInterface $request, ResponseInterface $response, array $args) {
+    
+        return $response->write("Hello, World");
+    
+    }
+
+}
+```
+
+## Setup/Config
+None!
+
+## Usage
+$app->get('/hello/world', '\Your\Namespace\HelloWorldAction');
+
+## Container
+These Action classes hold a container instance so you can receive dependencies
---
+++ src/BaseAction.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Glenn
+ * Date: 2015-12-21
+ * Time: 8:38 AM
+ */
+
+namespace TheSlimCollective\Helper;
+
+
+use Interop\Container\ContainerInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+abstract class BaseAction
+{
+    /**
+     * @var \Interop\Container\ContainerInterface
+     */
+    protected $container;
+
+    /**
+     * BaseAction constructor.
+     *
+     * @param \Interop\Container\ContainerInterface $containerInterface
+     */
+    public function __construct (ContainerInterface $containerInterface)
+    {
+        $this->container = $containerInterface;
+    }
+
+    /**
+     * @param \TheSlimCollective\Helper\string $name
+     * @return mixed
+     */
+    public function __get (string $name)
+    {
+        return $this->container->get($name);
+    }
+
+    /**
+     * @param \TheSlimCollective\Helper\string $name
+     * @return bool
+     */
+    public function __isset (string $name)
+    {
+        return $this->container->has($name);
+    }
+
+    /**
+     * @param \Psr\Http\Message\ServerRequestInterface $request
+     * @param \Psr\Http\Message\ResponseInterface      $response
+     * @param array                                    $args
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    public abstract function __invoke (ServerRequestInterface $request, ResponseInterface $response, array $args);
+}
